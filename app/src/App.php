e86dd4c77be75381df52a3d6b1512f400cd85394
Олег Кольжанов <o.kolzhanov@leko.team>
2023-07-20 01:09:40 +0300

    beejee test

--- app/src/App.php
+++ app/src/App.php
@@ -198,19 +198,6 @@ class App
         $this->dispatcher = simpleDispatcher(
             include APP_DIR . '/routes/app.php'
         );
-//        $this->dispatcher = simpleDispatcher(function (RouteCollector $r) {
-//            $r->addRoute('GET', '/', [TodoController::class, 'index']);
-//            $r->addRoute('GET', '/add', [TodoController::class, 'add']);
-//            $r->addRoute('POST', '/create', [TodoController::class, 'create']);
-//            $r->addRoute('GET', '/edit/{id}', [TodoController::class, 'edit']);
-//            $r->addRoute('POST', '/update/{id}', [TodoController::class, 'update']);
-//            $r->addRoute('POST', '/done', [TodoController::class, 'done']);
-//            $r->addRoute('GET', '/view/{id}', [TodoController::class, 'show']);
-//            $r->addRoute('GET', '/login', [AuthController::class, 'index']);
-//            $r->addRoute('POST', '/login', [AuthController::class, 'login']);
-//            $r->addRoute('GET', '/logout', [AuthController::class, 'logout']);
-//            $r->addRoute('POST', '/api/v1/get', [ApiController::class, 'index']);
-//        });
     }
 
     /**
